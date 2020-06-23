b911e43aa1ce2dae5073382a07a7358e163bebe6
Ben Latimer <latimerb@comet-ln2.sdsc.edu>
2020-06-23 12:10:55 -0700

    updated batch script

--- batch_mat.sh
+++ batch_mat.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-#SBATCH --partition compute
+#SBATCH --partition debug
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH -A TG-DBS180005
 #SBATCH --mem-per-cpu=5000
 #SBATCH --job-name=ca1
 #SBATCH --output=ca1%j.out
-#SBATCH --time 0-12:00
-#SBATCH --qos=normal
+#SBATCH --time 0-00:30
 #SBATCH -L matlab:24
 
 
 module load matlab/2019b
-matlab -nodesktop -nodisplay -nosplash < BuildNetworkFeng.m
+matlab -nodesktop -nodisplay -nosplash < BuildNetwork2.m
 
 
